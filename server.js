5db959d1fe23f886822ad65591627682ba12188e
Manas <mycodeunq@gmail.com>
2025-11-25 06:34:32 +0530

    1st commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 10000;
+
+app.get("/download", (req, res) => {
+  const filePath = path.resolve(__dirname, "mat.txt"); 
+
+  res.download(filePath, "myfile.txt", (err) => { 
+    if (err) {
+      console.error("Error while downloading:", err);
+      if (!res.headersSent) {
+        res.status(500).send("Error downloading the file");
+      }
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/download`);
+});
