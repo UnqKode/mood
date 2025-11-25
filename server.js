f9d1aaf60c4bb4d5ed2d1cc2bc4729c39d16e57f
Manas <mycodeunq@gmail.com>
2025-11-25 07:19:44 +0530

    3rd commit

--- server.js
+++ server.js
@@ -1,36 +1,19 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
 const app = express();
 const PORT = 10000;
 
 app.get("/download", (req, res) => {
-  const txtFilePath = path.resolve(__dirname, "mat.txt");
-  const mFilePath = path.resolve(__dirname, "mat.m");
+  const filePath = path.resolve(__dirname, "mat.m"); // MATLAB file
 
-  try {
-    // Read content from .txt file
-    const content = fs.readFileSync(txtFilePath, 'utf8');
-    
-    // Write content to .m file
-    fs.writeFileSync(mFilePath, content, 'utf8');
-    
-    console.log("✓ Converted mat.txt to mat.m");
-    
-    // Download the .m file
-    res.download(mFilePath, "mat.m", (err) => {
-      if (err) {
-        console.error("Error while downloading:", err);
-        if (!res.headersSent) {
-          res.status(500).send("Error downloading the file");
-        }
+  res.download(filePath, "script.m", (err) => { // downloaded as script.m
+    if (err) {
+      console.error("Error while downloading:", err);
+      if (!res.headersSent) {
+        res.status(500).send("Error downloading the file");
       }
-    });
-    
-  } catch (error) {
-    console.error("Error during conversion:", error);
-    res.status(500).send("Error converting or downloading file");
-  }
+    }
+  });
 });
 
 app.listen(PORT, () => {
